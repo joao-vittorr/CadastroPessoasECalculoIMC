9b593aa0592a48b5e1034e9af73e9969c830debf
João Vitor <vitorribeiro9454@gmail.com>
2023-10-01 18:33:37 -0300

    fazendo testes unitários

--- src/CadastroIMC.java
+++ src/CadastroIMC.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 class Pessoa {
     private String nome;
@@ -55,48 +57,78 @@ public class CadastroIMC {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Defina o nome de usuário e senha fixos
         String nomeUsuario = "admin";
         String senha = "senha123";
 
         System.out.println("Bem-vindo ao Sistema de Cadastro e IMC");
         System.out.print("Nome de Usuário: ");
-        String inputUsuario = scanner.nextLine();
+        String usuarioAdm = scanner.nextLine();
 
         System.out.print("Senha: ");
-        String inputSenha = scanner.nextLine();
+        String senhaAdm = scanner.nextLine();
 
-        if (inputUsuario.equals(nomeUsuario) && inputSenha.equals(senha)) {
+        if (usuarioAdm.equals(nomeUsuario) && senhaAdm.equals(senha)) {
             System.out.println("\nLogin bem-sucedido!");
             System.out.println("Agora você pode usar o sistema de cadastro e cálculo de IMC.");
 
-            System.out.println("\nCadastro de Pessoa");
-            System.out.print("Nome (obrigatório): ");
-            String nome = scanner.nextLine();
+            List<Pessoa> pessoas = new ArrayList<>(); // Lista para armazenar pessoas cadastradas
 
-            System.out.print("Email: ");
-            String email = scanner.nextLine();
+            while (true) { // Loop para exibir o menu
+                System.out.println("\nMenu:");
+                System.out.println("1. Cadastrar Pessoa");
+                System.out.println("2. Listar Pessoas Cadastradas");
+                System.out.println("3. Logout/Encerrar");
+                System.out.print("Escolha uma opção: ");
+                int escolha = scanner.nextInt();
 
-            System.out.print("Telefone: ");
-            String telefone = scanner.nextLine();
+                switch (escolha) {
+                    case 1:
+                        // Cadastro de Pessoa
+                        System.out.print("Nome (obrigatório): ");
+                        String nome = scanner.nextLine(); // Consumir a quebra de linha pendente
+                        nome = scanner.nextLine();
 
-            System.out.print("Idade (obrigatório): ");
-            int idade = scanner.nextInt();
+                        System.out.print("Email: ");
+                        String email = scanner.nextLine();
 
-            System.out.print("Peso (obrigatório): ");
-            double peso = scanner.nextDouble();
+                        System.out.print("Telefone: ");
+                        String telefone = scanner.nextLine();
 
-            System.out.print("Altura (obrigatório, use o ponto como separador decimal): ");
-            double altura = scanner.nextDouble();
+                        System.out.print("Idade (obrigatório): ");
+                        int idade = scanner.nextInt();
 
-            Pessoa pessoa = new Pessoa(nome, email, telefone, idade, peso, altura);
+                        System.out.print("Peso (obrigatório): ");
+                        double peso = scanner.nextDouble();
 
-            System.out.println("\nInformações da Pessoa:");
-            System.out.println(pessoa);
+                        System.out.print("Altura (obrigatório, use a vírgula como separador decimal, EX: 1,60): ");
+                        double altura = scanner.nextDouble();
+
+                        Pessoa pessoa = new Pessoa(nome, email, telefone, idade, peso, altura);
+                        pessoas.add(pessoa);
+
+                        System.out.println("Pessoa cadastrada com sucesso!");
+                        break;
+
+                    case 2:
+                        // Listar Pessoas Cadastradas
+                        System.out.println("\nPessoas Cadastradas:");
+                        for (Pessoa p : pessoas) {
+                            System.out.println(p);
+                        }
+                        break;
+
+                    case 3:
+                        // Sair do programa
+                        System.out.println("Logout realizado com Sucesso, programa encerrado!");
+                        scanner.close();
+                        System.exit(0);
+
+                    default:
+                        System.out.println("Opção inválida. Por favor, escolha novamente.");
+                }
+            }
         } else {
             System.out.println("Login falhou. Nome de usuário ou senha incorretos.");
         }
-
-        scanner.close();
     }
 }
